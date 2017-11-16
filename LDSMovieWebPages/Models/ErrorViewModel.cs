81d88a0fd432cc9a03824522f3f4434f33b0882a
stuart pennington <slp423@comcast.net>
2017-11-16 08:30:54 -0700

    Add project files.

---
+++ LDSMovieWebPages/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LDSMovieWebPages.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
