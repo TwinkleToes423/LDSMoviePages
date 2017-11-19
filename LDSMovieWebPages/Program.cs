bfa0080b8d79e23ba3e920bf1ecc944ba7d4bb18
stuart pennington <slp423@comcast.net>
2017-11-18 21:31:43 -0700

    added DB stuck on bug in Program.cs and SeedData.cs

--- LDSMovieWebPages/Program.cs
+++ LDSMovieWebPages/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using LDSMovieWebPages.Models;
 
 namespace LDSMovieWebPages
 {
@@ -14,7 +15,25 @@ namespace LDSMovieWebPages
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    // Requires using MvcMovie.Models;
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
--- LDSMovieWebPages/Startup.cs
+++ LDSMovieWebPages/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using LDSMovieWebPages.Models;
 
 namespace LDSMovieWebPages
 {
@@ -22,6 +24,9 @@ namespace LDSMovieWebPages
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddDbContext<LDSMovieWebPagesContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("LDSMovieWebPagesContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
