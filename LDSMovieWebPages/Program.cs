8f8adf3d632e51360b883e6aeb17382283b15ada
stuart pennington <slp423@comcast.net>
2017-11-18 23:16:08 -0700

    Fixed 2nd Program.cs bug.

--- LDSMovieWebPages/Program.cs
+++ LDSMovieWebPages/Program.cs
@@ -1,14 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using LDSMovieWebPages.Models;
 using MvcMovie.Models;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace LDSMovieWebPages
 {
